ad2b7076a28edd353e8fcf7d9f7ffd6b2b5cfe17
Kehan Qian <aud2024@bu.edu>
2025-11-09 15:50:25 -0500

    Update SurrClusterConformalDR.R

--- R/SurrClusterConformalDR.R
+++ R/SurrClusterConformalDR.R
@@ -91,8 +91,7 @@ SurrClusterConformalDR <- function(df,
       SL.library = SL.library
     )
 
-
-    q.Y1 <- cbind(
+#Produces out-of-sample lower/upper quantile predictions for all rows in df (still representing the A=1 world)
       predict(m.X1q1.obj, newdata = df[, grep("([X])", colnames(df),
                                               value = TRUE
       ),
